9e693b87567bd6cd1b8d5a9075a597b05ebe396f
Julien <julien.le.coupanec@gmail.com>
2018-09-13 03:14:59 +0200

    docs(index): display icons

--- pages/index.vue
+++ pages/index.vue
@@ -137,6 +137,17 @@
             class="c-index__divider"
           )
 
+    div(
+      v-if="section.icons"
+      class="c-index__elements c-index__elements--icons"
+    )
+      base-icon(
+        v-for="icon in section.icons"
+        :key="icon"
+        :name="icon"
+        color="white"
+      )
+
     base-divider(
       color="black"
       size="large"
@@ -153,6 +164,9 @@ import CommonColor from "@/components/common/CommonColor";
 import CommonHeadlines from "@/components/common/CommonHeadlines";
 import TheGithubButtons from "@/components/layouts/TheGithubButtons";
 import TheLogo from "@/components/layouts/TheLogo";
+
+import colors from "@/schemas/colors";
+import icons from "@/schemas/icons";
 
 export default {
   components: {
@@ -172,100 +186,7 @@ export default {
             description: "Gather all colors here in a beautiful way",
             hasDocumentation: false
           },
-          colors: [
-            {
-              hex: "#AB7DF6",
-              name: "$portage"
-            },
-            {
-              hex: "#0079C4",
-              name: "$lochmara"
-            },
-            {
-              hex: "#0093EE",
-              name: "$azure-radiance"
-            },
-            {
-              hex: "#26C1C9",
-              name: "$java"
-            },
-            {
-              hex: "#40B630",
-              name: "$apple"
-            },
-            {
-              hex: "#4ECE3D",
-              name: "$apple2"
-            },
-            {
-              hex: "#81C926",
-              name: "$atlantis"
-            },
-            {
-              hex: "#E0102B",
-              name: "$crimson"
-            },
-            {
-              hex: "#EE334B",
-              name: "$amaranth"
-            },
-            {
-              hex: "#FD7B1F",
-              name: "$pumpkin"
-            },
-            {
-              hex: "#FACA00",
-              name: "$supernova"
-            },
-            {
-              hex: "#FFF5CC",
-              name: "$barley-white"
-            },
-            {
-              hex: "#FFFFFF",
-              name: "$white"
-            },
-            {
-              hex: "#FAFBFC",
-              name: "$athens-gray"
-            },
-            {
-              hex: "#ECF5FD",
-              name: "$selago"
-            },
-            {
-              hex: "#C5D9E8",
-              name: "$periwinkle"
-            },
-            {
-              hex: "#A8C6DF",
-              name: "$regent-st-blue"
-            },
-            {
-              hex: "#8DABC4",
-              name: "$nepal"
-            },
-            {
-              hex: "#3F536E",
-              name: "$fiord"
-            },
-            {
-              hex: "#2C405A",
-              name: "$rhino"
-            },
-            {
-              hex: "#273950",
-              name: "$rhino2"
-            },
-            {
-              hex: "#25374F",
-              name: "$rhino3"
-            },
-            {
-              hex: "#1C293B",
-              name: "$mirage"
-            }
-          ]
+          colors: colors
         },
         {
           headlines: {
@@ -345,11 +266,12 @@ export default {
         },
         {
           headlines: {
-            title: "200+ ICONS FROM MATERIAL DESIGN 👻",
+            title: "900+ ICONS FROM MATERIAL DESIGN 👻",
             description:
               "Material icons are delightful, beautifully crafted symbols for common actions and items.",
             hasDocumentation: true
-          }
+          },
+          icons: icons
         }
       ]
     };
@@ -446,6 +368,11 @@ $c: ".c-index";
       #{$c}__divider {
         margin: 0 auto;
       }
+    }
+
+    &--icons {
+      grid-gap: 20px;
+      grid-template-columns: repeat(auto-fill, 24px);
     }
 
     &--social-auths {
