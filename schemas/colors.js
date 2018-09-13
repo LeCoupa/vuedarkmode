8a416237a3d4c2ff710440fb2031f9e90cbcf55c
Julien <julien.le.coupanec@gmail.com>
2018-09-13 03:13:18 +0200

    refactoring(colors): specific file

---
+++ schemas/colors.js
@@ -0,0 +1,104 @@
+/**************************************************************************
+ * COLORS DEFINITION
+ ***************************************************************************/
+
+const colors = [
+  {
+    hex: "#AB7DF6",
+    name: "$portage"
+  },
+  {
+    hex: "#0079C4",
+    name: "$lochmara"
+  },
+  {
+    hex: "#0093EE",
+    name: "$azure-radiance"
+  },
+  {
+    hex: "#26C1C9",
+    name: "$java"
+  },
+  {
+    hex: "#40B630",
+    name: "$apple"
+  },
+  {
+    hex: "#4ECE3D",
+    name: "$apple2"
+  },
+  {
+    hex: "#81C926",
+    name: "$atlantis"
+  },
+  {
+    hex: "#E0102B",
+    name: "$crimson"
+  },
+  {
+    hex: "#EE334B",
+    name: "$amaranth"
+  },
+  {
+    hex: "#FD7B1F",
+    name: "$pumpkin"
+  },
+  {
+    hex: "#FACA00",
+    name: "$supernova"
+  },
+  {
+    hex: "#FFF5CC",
+    name: "$barley-white"
+  },
+  {
+    hex: "#FFFFFF",
+    name: "$white"
+  },
+  {
+    hex: "#FAFBFC",
+    name: "$athens-gray"
+  },
+  {
+    hex: "#ECF5FD",
+    name: "$selago"
+  },
+  {
+    hex: "#C5D9E8",
+    name: "$periwinkle"
+  },
+  {
+    hex: "#A8C6DF",
+    name: "$regent-st-blue"
+  },
+  {
+    hex: "#8DABC4",
+    name: "$nepal"
+  },
+  {
+    hex: "#3F536E",
+    name: "$fiord"
+  },
+  {
+    hex: "#2C405A",
+    name: "$rhino"
+  },
+  {
+    hex: "#273950",
+    name: "$rhino2"
+  },
+  {
+    hex: "#25374F",
+    name: "$rhino3"
+  },
+  {
+    hex: "#1C293B",
+    name: "$mirage"
+  }
+];
+
+/**************************************************************************
+ * EXPORTS
+ ***************************************************************************/
+
+export default colors;
