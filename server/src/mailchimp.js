9efcc1fab20300942774e6e74944f80748c4ee36
Julien <julien.le.coupanec@gmail.com>
2018-10-16 22:47:25 +0200

    feat(mailchimp): use the MAILCHIMP_API_KEY from env

--- server/src/mailchimp.js
+++ server/src/mailchimp.js
@@ -1,10 +1,37 @@
 /**************************************************************************
+ * IMPORTS
+ ***************************************************************************/
+
+// NPM
+import axios from "axios";
+import dotenv from "dotenv";
+import querystring from "querystring";
+
+/**************************************************************************
  * SUBSCRIBE EMAIL ADDRESS TO MAILCHIMP
  ***************************************************************************/
 
-exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
-  });
+// TODO: Use the MAILCHIMP_API_KEY from env
+// TODO: Verify email is right
+// TODO: Trigger message when email added
+// TODO: Trigger message when email were already added previously
+// TODO: Return any error sent by Mailchimp
+exports.handler = async function(event, context, callback) {
+  // Only allow POST
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  // Load environment variables
+  dotenv.config();
+
+  const bodyParams = querystring.parse(event.body);
+
+  await axios.post(
+    "https://us19.api.mailchimp.com/3.0/lists/00f5d5e482/members/",
+    { email_address: bodyParams.email, status: "subscribed" },
+    { auth: { username: "anystring", password: process.env.MAILCHIMP_API_KEY } }
+  );
+
+  callback(null, { statusCode: 200, body: bodyParams.email });
 };
