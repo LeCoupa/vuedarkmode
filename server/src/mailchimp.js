96fb3c96f915cc5a9f901456c13ab41776b6c46f
Julien <julien.le.coupanec@gmail.com>
2018-10-16 23:08:55 +0200

    feat(mailchimp): verify if email is right

--- server/src/mailchimp.js
+++ server/src/mailchimp.js
@@ -11,27 +11,46 @@ import querystring from "querystring";
  * SUBSCRIBE EMAIL ADDRESS TO MAILCHIMP
  ***************************************************************************/
 
-// TODO: Use the MAILCHIMP_API_KEY from env
+// Load environment variables
+dotenv.config();
+
 // TODO: Verify email is right
 // TODO: Trigger message when email added
 // TODO: Trigger message when email were already added previously
 // TODO: Return any error sent by Mailchimp
-exports.handler = async function(event, context, callback) {
-  // Only allow POST
+exports.handler = async function(event) {
+  const bodyParams = querystring.parse(event.body);
+
+  // Only allow POST method
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
 
-  // Load environment variables
-  dotenv.config();
+  // Only accept valid email
+  const re = /[a-z0-9\._%+!$&*=^|~#%'`?{}/\-]+@([a-z0-9\-]+\.){1,}([a-z]{2,16})/;
 
-  const bodyParams = querystring.parse(event.body);
+  if (!re.test(bodyParams.email)) {
+    return {
+      statusCode: 400,
+      body: "This is not a valid email"
+    };
+  }
 
-  await axios.post(
-    "https://us19.api.mailchimp.com/3.0/lists/00f5d5e482/members/",
-    { email_address: bodyParams.email, status: "subscribed" },
-    { auth: { username: "anystring", password: process.env.MAILCHIMP_API_KEY } }
-  );
+  // Add valid email to the Mailchimp list
+  try {
+    await axios.post(
+      "https://us19.api.mailchimp.com/3.0/lists/00f5d5e482/members/",
+      { email_address: bodyParams.email, status: "subscribed" },
+      {
+        auth: { username: "anystring", password: process.env.MAILCHIMP_API_KEY }
+      }
+    );
+  } catch (error) {
+    // return {};
+  }
 
-  callback(null, { statusCode: 200, body: bodyParams.email });
+  return {
+    statusCode: 200,
+    body: "You are now subscribed to the newsletter."
+  };
 };
