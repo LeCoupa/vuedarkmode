51ab0288e3948b5ec2d70a7ec5cf78ccbbf1b97e
Julien <julien.le.coupanec@gmail.com>
2018-10-30 16:50:57 +0100

    fix(base-avatar): better validator for cursor

--- components/darkmode/base/BaseAvatar.vue
+++ components/darkmode/base/BaseAvatar.vue
@@ -74,7 +74,7 @@ export default {
       type: String,
       default: "default",
       validator(x) {
-        return ["default", "pointer"];
+        return ["default", "pointer"].indexOf(x) !== -1;
       }
     },
     description: {
