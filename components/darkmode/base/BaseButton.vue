e0ab076d15995c709c32274cbf187eca0ad7d385
Julien <julien.le.coupanec@gmail.com>
2018-10-31 22:41:37 +0100

    feat(base-button): add mouseenter and mouseleave

--- components/darkmode/base/BaseButton.vue
+++ components/darkmode/base/BaseButton.vue
@@ -7,6 +7,8 @@ button(
   @click="onClick(id, $event)"
   @dblclick="onDoubleClick(id, $event)"
   @mousedown="onMouseDown(id, $event)"
+  @mouseenter="onMouseEnter(id, $event)"
+  @mouseleave="onMouseLeave(id, $event)"
   @mousemove="onMouseMove(id, $event)"
   @mouseout="onMouseOut(id, $event)"
   @mouseover="onMouseOver(id, $event)"
@@ -165,6 +167,14 @@ export default {
 
     onMouseDown(id, event) {
       this.$emit("mousedown", id, event);
+    },
+
+    onMouseEnter(id, event) {
+      this.$emit("mouseenter", id, event);
+    },
+
+    onMouseLeave(id, event) {
+      this.$emit("mouseleave", id, event);
     },
 
     onMouseMove(id, event) {
