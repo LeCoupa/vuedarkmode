5212292965f76c28b65e0e888a944789301a8ddf
Julien <julien.le.coupanec@gmail.com>
2018-11-18 22:36:55 +0100

    docs(guide-package-options): update with new components

--- components/guide/GuidePackageOptions.vue
+++ components/guide/GuidePackageOptions.vue
@@ -14,7 +14,7 @@
       | Vue.use(VueDarkMode, {
       |   // Specify the components to declare globally in your project
       |   // When undefined, null or given an empty array, all components will be imported
-      |   components: ["avatar", "badge", "button", "divider", "heading", "icon",  "progress-bar",  "social-login", "checkbox", "file", "input", "radio", "select", "tabs", "textarea", "toggle"]
+      |   components: ["alert", "avatar", "badge", "button", "divider", "heading", "icon",  "progress-bar",  "social-login", "spinner", "checkbox", "file", "input", "radio", "select", "tabs", "textarea", "toggle"]
       | });
 </template>
 
