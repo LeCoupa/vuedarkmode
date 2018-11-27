544170ffb8140081d8108000409acdf98229593c
Julien <julien.le.coupanec@gmail.com>
2018-11-27 19:32:30 +0100

    style(base-list-item): decrease space between content and source

--- components/darkmode/base/BaseListItem.vue
+++ components/darkmode/base/BaseListItem.vue
@@ -154,7 +154,7 @@ $colors: black, blue, green, red, orange, white;
   #{$c}__source {
     display: flex;
     align-items: center;
-    margin-top: 6px;
+    margin-top: 2px;
     padding-left: 60px;
 
     #{$c}__icon {
