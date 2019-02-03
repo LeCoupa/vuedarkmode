f29a35b4665ecf1b1e6b34c6a44dc6e17ed327a0
Julien <julien.le.coupanec@gmail.com>
2019-02-03 18:55:28 +0100

    build(app): set eslint rules to strongly-recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    "plugin:vue/recommended"
+    "plugin:vue/strongly-recommended"
   ],
   // add your custom rules here
   rules: {
