df53649c9b6722a81213dfd383fe70d45f65ae49
Julien Le Coupanec <julien.le.coupanec@gmail.com>
2019-05-25 22:49:48 +0200

    docs(guide-field-select): add slots

--- docs/components/guide/GuideFieldSelects.vue
+++ docs/components/guide/GuideFieldSelects.vue
@@ -42,7 +42,9 @@
       code(class="html")
         | &lt;!-- Insert this component in your code --&gt;
         | &lt;!-- Customize it with props (see table below) --&gt;
-        | &lt;dm-select v-model="synchronizedValue"&gt;&lt;/dm-select&gt;
+        |   &lt;template #option-left="{ option }"&gt;&lt;/template&gt;
+        |   &lt;template #option-right="{ option }"&gt;&lt;/template&gt;
+        | &lt;/dm-select&gt;
 
     no-ssr
       common-table(
