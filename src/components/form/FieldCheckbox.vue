c56cbacd01156f8724fb42a7936a47efe0bfb2fa
Julien Le Coupanec <julien.le.coupanec@gmail.com>
2019-11-15 21:49:29 +0100

    fix(field-checkbox): increase margin top for label

--- src/components/form/FieldCheckbox.vue
+++ src/components/form/FieldCheckbox.vue
@@ -155,7 +155,7 @@ $statuses: "error", "normal", "success", "warning";
 
     #{$c}__label {
       flex: 1;
-      margin-top: 1px;
+      margin-top: 2px;
       margin-bottom: 0;
       font-weight: 400;
     }
