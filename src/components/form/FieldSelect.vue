0d6747dc63d89c3ea371415ad63451d24b32f199
Julien Le Coupanec <julien.le.coupanec@gmail.com>
2019-12-22 12:20:55 +0100

    style(field-select): rework default function

--- src/components/form/FieldSelect.vue
+++ src/components/form/FieldSelect.vue
@@ -202,9 +202,7 @@ export default {
     },
     options: {
       type: Array,
-      default() {
-        return []
-      }
+      default: () => []
     },
     placeholder: {
       type: String,
