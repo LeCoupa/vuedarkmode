cd797428f856e5a9e310127dec2e50713404eea7
Julien Le Coupanec <julien.le.coupanec@gmail.com>
2020-01-31 14:13:27 +0100

    feat(field-mixin): add validations

--- src/mixins/FieldMixin.js
+++ src/mixins/FieldMixin.js
@@ -130,28 +130,89 @@ export default {
       }
     },
 
+    // Available rules: https://vuelidate.js.org/#sub-builtin-validators
     validationMessage() {
       let message = ""
 
       if (this.validation && this.validation.$dirty) {
-        if (this.validation.required === false) {
-          message = "This field is required."
-        } else if (this.validation.email === false) {
-          message = "This field is not a valid email."
-        } else if (this.validation.minLength === false) {
+        // Required, Required If, Required Unless
+        if (
+          this.validation.required === false ||
+          this.validation.requiredIf === false ||
+          this.validation.requiredUnless === false
+        ) {
+          message = "The value is required."
+        }
+        // Min Length
+        else if (this.validation.minLength === false) {
           const min = this.validation.$params.minLength.min
-          message = `This field is too short (min: ${min}).`
-        } else if (this.validation.maxLength === false) {
-          const max = this.validation.$params.minLength.max
-          message = `This field is too long (max ${max}).`
-        } else if (this.validation.sameAs === false) {
+          message = `The value is too short (min: ${min}).`
+        }
+        // Max Length
+        else if (this.validation.maxLength === false) {
+          const max = this.validation.$params.maxLength.max
+          message = `This value is too long (max ${max}).`
+        }
+        // Min Value
+        else if (this.validation.minValue === false) {
+          const min = this.validation.$params.minValue.min
+          message = `Minimum value allowed: ${min}.`
+        }
+        // Max Value
+        else if (this.validation.maxValue === false) {
+          const max = this.validation.$params.maxValue.max
+          message = `Maximum value allowed: ${max}.`
+        }
+        // Between
+        else if (this.validation.between === false) {
+          const max = this.validation.$params.between.max
+          const min = this.validation.$params.between.min
+          message = `Value should be between ${min} and ${max}.`
+        }
+        // Alpha
+        else if (this.validation.alpha === false) {
+          message = "The value accepts only alphabet characters."
+        }
+        // Alpha Num
+        else if (this.validation.alphaNum === false) {
+          message = "The value accepts only alphanumerics."
+        }
+        // Numeric
+        else if (this.validation.numeric === false) {
+          message = "The value accepts only numerics."
+        }
+        // Integer
+        else if (this.validation.integer === false) {
+          message = "The value accepts only positive and negative integers."
+        }
+        // Integer
+        else if (this.validation.decimal === false) {
+          message = "The value accepts only positive and negative decimal numbers."
+        }
+        // Email
+        else if (this.validation.email === false) {
+          message = "The value is not a valid email."
+        }
+        // IP Address
+        else if (this.validation.ipAddress === false) {
+          message = "The value accepts only valid IPv4 addresses."
+        }
+        // Mac Address
+        else if (this.validation.macAddress === false) {
+          message = "The value accepts only valid MAC addresses."
+        }
+        // Same As
+        else if (this.validation.sameAs === false) {
           const field = this.validation.$params.sameAs.eq
-          message = `This field does not match: ${field}.`
-        } else if (this.validation.$invalid === true) {
-        } else if (this.validation.url === false) {
-          message = "This field is not a valid url."
-        } else if (this.validation.$invalid === true) {
-          message = "This field is invalid."
+          message = `The value does not match: ${field}.`
+        }
+        // Url
+        else if (this.validation.url === false) {
+          message = "The value is not a valid url."
+        }
+        // Other rules
+        else if (this.validation.$invalid === true) {
+          message = "The value is invalid."
         }
       }
 
